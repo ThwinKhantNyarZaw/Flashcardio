d2cab061758df03e40a4d7acacb684256651b2fe
ThwinKhantNyarZaw <156641445+ThwinKhantNyarZaw@users.noreply.github.com>
2025-04-24 13:29:42 +0000

    final

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoMC6SE58fC58uDB0fbtU1YyigLE5DDvU",
-  authDomain: "flashcardio-11.firebaseapp.com",
-  projectId: "flashcardio-11",
-  storageBucket: "flashcardio-11.firebasestorage.app",
-  messagingSenderId: "669106215792",
-  appId: "1:669106215792:web:b1e9240a314abc7d6bcdb5"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
